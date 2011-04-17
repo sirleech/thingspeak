701c0f832e1e29a286e5c084fe8870b5fd6857f3
Christopher Lee <chris@australianrobotics.com.au>
2011-04-18 07:57:12 +1000

    catch socket.error exception

--- thingspeak_cpu_loop.py
+++ thingspeak_cpu_loop.py
@@ -11,11 +11,15 @@ def doit():
 	params = urllib.urlencode({'field1': cpu_pc, 'field2': mem_avail_mb,'key':'YOUR_API_KEY'})
 	headers = {"Content-type": "application/x-www-form-urlencoded","Accept": "text/plain"}
 	conn = httplib.HTTPConnection("api.thingspeak.com:80")
-	conn.request("POST", "/update", params, headers)
-	response = conn.getresponse()
-	print response.status, response.reason
-	data = response.read()
-	conn.close()
+	
+	try:
+		conn.request("POST", "/update", params, headers)
+		response = conn.getresponse()
+		print response.status, response.reason
+		data = response.read()
+		conn.close()
+	except socket.error:
+		"socket error, connection failed"	
 
 #sleep for 16 seconds (api limit of 15 secs)
 if __name__ == "__main__":
